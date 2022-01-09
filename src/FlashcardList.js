86a484de9702d5faa6449348ecb6f3bd170f964e
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-09 20:15:34 +0300

    final release

---
+++ src/FlashcardList.js
@@ -0,0 +1,11 @@
+import { Flashcard } from "./Flashcard";
+
+export const FlashcardList = ({ flashcards }) => {
+  return (
+    <div className="card-grid">
+      {flashcards.map((flashcard) => {
+        return <Flashcard flashcard={flashcard} key={flashcard.id} />;
+      })}
+    </div>
+  );
+};
